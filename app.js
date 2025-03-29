30bd7683802f83a69c256799703800b806e0d859
honeybunnysidd <60231540@iimtindia.net>
2025-03-29 18:26:34 +0530

    add middleware

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const ExpressError = require("./ExpressError");
 
 //Middleware --->
 /*
@@ -21,6 +22,19 @@ app.use((req, res, next) => {
   next();
 });
 */
+let checkToken = (req, res, next) => {
+  let { token } = req.query;
+  if (token == "secret") {
+    next();
+  } else {
+    //Throw Error
+    throw new ExpressError(401, "Shi Kro ise bhai");
+  }
+};
+
+app.get("/api", checkToken, (req, res) => {
+  res.send("This is your api");
+});
 
 app.use("/random", (req, res, next) => {
   console.log(req.method);
@@ -32,6 +46,25 @@ app.get("/", (req, res) => {
 });
 app.get("/random", (req, res) => {
   res.send("I am random page");
+});
+//Activity
+app.use("/admin", (req, res, next) => {
+  throw new ExpressError(403, "Access denied");
+});
+app.get("/admin", (req, res) => {
+  res.send("OK");
+});
+
+//Error Handling
+
+app.get("/error", (req, res) => {
+  stupid != stupid;
+});
+
+app.use((err, req, res, next) => {
+  let { status = 505, message } = err;
+  console.log(`---------------Error---------------`);
+  res.status(status).send(message);
 });
 
 app.listen(8080, () => {
